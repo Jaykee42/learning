3f99c93da43e6cf40606a4992651d94c1fd5c340
Jaykee <jaykee422@gmail.com>
2023-10-18 22:26:06 +0300

    Start stack

---
+++ Learning/algorithm and data type/stack/main.c
@@ -0,0 +1,18 @@
+/*realese stack. main array and two func: pop and push*/
+
+#include <stdio.h>
+
+int *pop1, *push1, stack[50];
+
+
+int main(){
+
+    push1 = stack;
+    push(5);
+    printf("%d ", push1[0]);
+}
+
+void push(int i) {
+    push1++;
+    *push1 == i;
+}
