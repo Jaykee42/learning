bc7d842f4b3f8017147a73404fe39214809eb650
Jaykee <jaykee422@gmail.com>
2023-11-24 00:11:45 +0300

    Finish a task

--- leetcode/TwoSums/main.c
+++ leetcode/TwoSums/main.c
@@ -1,25 +1,29 @@
 /**
- Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
- You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
- You can return the answer in any order.
- 
  * Note: The returned array must be malloced, assume caller calls free().
  */
-
 #include <stdio.h>
+int* twoSum(int* nums, int numsSize, int target) {
 
-int testArray[] = {2,3,5,1,6,7,9,0};
-int *p = testArray;
+    int *array = malloc(2*sizeof(int) );
+    for (int i=0; i<numsSize;i++)
+{
+    for( int j=1+i;j<numsSize;j++)
+    {
+        if(nums[i] + nums[j] == target)
+        {
+        array[0]=i;
+        array[1]=j;
+        return array;
+        }
+    }   
+    
+}
+return -1;
+
+}
 
 int main() {
-   
-    int arrayMax = sizeof(p) / sizeof(p[0]);
-    for (int i = 0; i < 8; i++) {
-        if (p[i] + p[++i] == 8) {
-            printf("%d", 0);
-        }
-        else printf("%d", 1);
-    }
+    int cock[] = {2,7,11,15};
+    int test = twoSum(cock, 4, 9);
+    printf(test);
 }
