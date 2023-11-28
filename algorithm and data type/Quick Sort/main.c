4a1c908a872d8b055e0c7c2abb18e9aa894123ac
Jaykee <jaykee422@gmail.com>
2023-11-28 14:33:19 +0300

    Algo: Quick Sort: add empty funcs

--- algorithm and data type/Quick Sort/main.c
+++ algorithm and data type/Quick Sort/main.c
@@ -8,10 +8,18 @@ void partision(int* arr[], int* low, int* high) {
     for (int j = *low; j < *high; j++) {
         if (arr[j] < pivot) {
             i++;
+            swap(arr[i], arr[j]);
         }
     }
 }
 
+int quickSort() {
+
+}
+
+int swap() {
+
+}
 
 int main() {
 
