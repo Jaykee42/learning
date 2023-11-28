02c0a9894671a5495e225e9b32ed20641ae3ba67
Jaykee <jaykee422@gmail.com>
2023-11-28 22:11:49 +0300

    Algo: QuickSort: almost complete (need to add a normal input)

--- algorithm and data type/Quick Sort/main.c
+++ algorithm and data type/Quick Sort/main.c
@@ -1,26 +1,44 @@
 #include <stdio.h>
 
+int testarr[1000] = {67, 556, 345, 77, 989, 56, 33, 456, 23, 656565, 9898, 99, 6778, 7, 55656};
 
+void swap(int* a, int* b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
 
-void partision(int* arr[], int* low, int* high) {
-    int pivot = arr[*high];
-    int i = *low - 1;
-    for (int j = *low; j < *high; j++) {
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
         if (arr[j] < pivot) {
             i++;
-            swap(arr[i], arr[j]);
+            swap(&arr[i], &arr[j]);
         }
+    }
+
+    swap(&arr[i + 1], &arr[high]);
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivotIndex = partition(arr, low, high);
+        quickSort(arr, low, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, high);
     }
 }
 
-int quickSort() {
-
-}
-
-int swap() {
-
-}
-
 int main() {
+    int n = sizeof(testarr) / sizeof(testarr[0]);
 
+    quickSort(testarr, 0, n - 1);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", testarr[i]);
+    }
+
+    return 0;
 }
