4d7f5fb976b5330b6bc032c7d89409fb2337e3d1
Jaykee <jaykee422@gmail.com>
2023-11-30 15:08:36 +0300

    Algo: Linear Search: Add input

--- algorithm and data type/Linear Search/main.c
+++ algorithm and data type/Linear Search/main.c
@@ -3,16 +3,26 @@
 int tesarr[10] = {5,7,3,34,65,78,2524,1,65,897};
 
 void linearSearh(int arr[], int to_search) {
+    int founded = 0;
     for (int i = 0; i < 10; i++) {
         if (arr[i] == to_search) {
             printf("Value is founded:\n");
             printf("%d ", arr[i-1]);
             printf("[%d] ", arr[i]);
-            printf("%d ", arr[i+1]);
+            printf("%d \n", arr[i+1]);
+            founded = 1;
         }
+        
+    }
+    if (!founded) {
+        printf("Not founded.");
     }
 }
 
 int main() {
-    linearSearh(tesarr, 2524);
+    int value, n;
+    printf("Enter the number to find: \n");
+    scanf("%d", &value);
+    linearSearh(tesarr, value);
+    scanf("%d", n);
 }
