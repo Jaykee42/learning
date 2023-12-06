d341925f0cb6e9ca8c9ac9aae3eb1f71266ea7d0
Jaykee <jaykee422@gmail.com>
2023-12-06 13:50:26 +0300

    Task planner: add action func

--- To-Do Planner/main.c
+++ To-Do Planner/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 int task_index[100];
+char choice;
+
+void action() {
+    char input;
+    scanf("%c", &input);
+    switch (input)
+    {
+    case 'a':
+        choice = 'a';
+        break;
+    
+    default:
+        break;
+    }
+}
 
 void addTask() {
     task_index[0] = 1;
@@ -15,6 +30,7 @@ void showTasks() {
 }
 
 int main() {
+
     showTasks();
     addTask();
     showTasks();
