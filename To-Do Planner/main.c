c825cc42c835045adcc04149f523e95169ddfbf9
Jaykee <jaykee422@gmail.com>
2023-12-10 15:40:46 +0300

    To-Do Planner: add struct for task

--- To-Do Planner/main.c
+++ To-Do Planner/main.c
@@ -2,48 +2,52 @@
 
 #define MAX_TASKS 10000
 
-int task_index[MAX_TASKS];
+struct task {
+    int index;
+    char name[30];
+};
+
+struct task task_array[MAX_TASKS];
 int task_count = 0;
 
 void addTask() {
     if (task_count < MAX_TASKS) {
-        task_index[task_count] = 1;
+        printf("Enter new task name: \n");
+        scanf("%s", task_array[task_count].name);
+        task_array[task_count].index = task_count + 1;
         task_count++;
         printf("Task added!\n");
+    } else {
+        printf("Task limit reached. Can't add more tasks.\n");
     }
-    else {
-        printf("task limit reached. Can't add more tasks.\n");
-    }
-    
 }
 
 void removeTask() {
     if (task_count > 0) {
-        task_index[task_count] = 0;
         task_count--;
         printf("Task removed.\n");
-    }
-    else {
+    } else {
         printf("No tasks to remove.\n");
     }
-    
-
 }
 
 void showTasks() {
-    
-    for (int i = 0; i < task_count; i++) {
-        printf("%d\n", task_index[i]);
+    if (task_count == 0) {
+        printf("Task list is empty\n");
+    } else {
+        printf("List of tasks:\n");
+        for (int i = 0; i < task_count; i++) {
+            printf("Task %d: %s\n", task_array[i].index, task_array[i].name);
+        }
     }
 }
 
 int main() {
     char input;
-    
-    while (input != '0')
-    {
-        printf("Enter the action: ");
-        scanf(" %c", &input);  
+
+    while (input != '0') {
+        printf("Enter the action (a - add task; s - remove, d - show tasks, 0 - exit): ");
+        scanf(" %c", &input);
         switch (input) {
         case 'a':
             addTask();
@@ -59,9 +63,10 @@ int main() {
         case '0':
             break;
         default:
+            printf("Invalid choice. Try again.\n");
             break;
         }
-
     }
-     
+
+    return 0;
 }
