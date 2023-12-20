4d6863e13e9ac8b8d7400ef89e876d358bd272b9
Jaykee <jaykee422@gmail.com>
2023-12-20 13:52:26 +0300

    Algo: Add text to file: rebuild main func

--- algorithm and data type/Add and Remove text/main.c
+++ algorithm and data type/Add and Remove text/main.c
@@ -2,12 +2,28 @@
 #include <stdio.h>
 
 
-int main() {
-    FILE *fp;
-    fp = fopen("test.txt", "w");
-    char ch = getchar();
-    putc(ch, fp);
-    fclose;
+int main(int argc, char *argv[]) {
+
+    FILE *file_pointer;
+    char input;
+
+    if(argc!=2) {
+        printf("Error: Please input a file name");
+        exit(1);
+    }
+
+    if((file_pointer=fopen(argv[1], "w")) == NULL) {
+        printf("Error while open the file");
+    }
+
+    do {
+        input = getchar();
+        putc(input, file_pointer);
+    } while (input!='$');
+
+    fclose(file_pointer);
+
+    return 0;
 }
 
 
