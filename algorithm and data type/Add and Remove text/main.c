03fe5b5fb4b150f34ea62ac54ddd2f707f8d80ad
Jaykee <jaykee422@gmail.com>
2023-12-24 20:40:28 +0300

    Algo: Add text to file: reduild func add test (make default file)

--- algorithm and data type/Add and Remove text/main.c
+++ algorithm and data type/Add and Remove text/main.c
@@ -2,31 +2,27 @@
 #include <stdio.h>
 #include <string.h>
 
+#define DEFAULT_FILE_NAME "test.txt"
 
 int main(int argc, char *argv[]) {
 
     FILE *file_pointer;
     char input[90];
 
-    if(argc!=2) {
-        printf("Error: Please input a file name");
-        exit(1);
-    }
+    char *filename = (argc == 2) ? argv[1] : DEFAULT_FILE_NAME;
 
-    if((file_pointer=fopen(argv[1], "w")) == NULL) {
-        printf("Error while open the file");
+    if ((file_pointer = fopen(filename, "w")) == NULL) {
+        printf("Error while opening the file\n");
+        exit(1);
     }
 
     do {
         printf("Enter a string: ");
-        gets(input);
-        strcat(input, "\n");
+        fgets(input, sizeof(input), stdin);
         fputs(input, file_pointer);
-    } while (*input!='\n');
+    } while (*input != '\n');
 
-    
+    fclose(file_pointer);
 
     return 0;
 }
-
-
