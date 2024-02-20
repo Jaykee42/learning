5fea025856c8a07bd1c458cafbb20ea74c01ff9b
Jaykee <jaykee422@gmail.com>
2024-02-20 15:06:07 +0300

    SpaceRogue: start to move

--- SpaceRogue/Linux/main_curses.c
+++ SpaceRogue/Linux/main_curses.c
@@ -1,8 +1,25 @@
 #include <ncurses.h>
 
 int main() {
-	initsc();
-	mvaddch(22, 10, '@');
+	int c;
+	int y = 11, x = 11;
+	
+	
+	initscr();
+	noecho();
+	keypad(stdscr, 1); //allow arrows
+	curs_set(0);
+	
+	
+	do {
+		if (c == KEY_UP) y--;
+		else if (c == KEY_DOWN) y++;
+		else if (c == KEY_LEFT) x--;
+		else if (c == KEY_RIGHT) x++;
+		mvaddch(y, x, '@');
+	} while ((c = getch()) != 27); // 27 - ESC
+		
+	
 	getch();
 	endwin();
 	return 0;
