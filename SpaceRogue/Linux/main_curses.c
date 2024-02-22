ba51432b25d833f0cdf37b74bdafb477c93b0a55
Jaykee <jaykee422@gmail.com>
2024-02-22 15:06:54 +0300

    Space Rogue: add wals prototype

--- SpaceRogue/Linux/main_curses.c
+++ SpaceRogue/Linux/main_curses.c
@@ -3,16 +3,19 @@
 int main() {
 	int c;
 	int y = 11, x = 11;
-	
+	int cols, rows;
 	
 	initscr();
 	noecho();
 	keypad(stdscr, 1); //allow arrows
 	curs_set(0);
-	
+	getmaxyx(stdscr, rows, cols);
 	
 	do {
-		clear();
+		for (int yy = 0; yy <= rows; yy++)
+			for (int xx = 0; xx <= cols; xx++)
+				mvaddch(yy, xx, '#');
+		printw("rows - %d, cols - %d", rows, cols);
 		if (c == KEY_UP) y--;
 		else if (c == KEY_DOWN) y++;
 		else if (c == KEY_LEFT) x--;
