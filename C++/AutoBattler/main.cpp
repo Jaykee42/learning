fc68c99bdc8996fbc8967ab666f311e729fa8a7d
Jaykee <jaykee422@gmail.com>
2024-04-17 15:07:28 +0300

    Autobattler: addd battle func

--- C++/AutoBattler/main.cpp
+++ C++/AutoBattler/main.cpp
@@ -3,81 +3,99 @@ using namespace std;
 
 string playerCharacterName;
 
-void printGameField () {
-    int screenX = 10; 
-    int screenY = 20;
-    for (int x = 0; x < screenX; x++) {
-        for (int y = 0; y < screenY; y++)
-        {
-            if (x == 1 || x == screenX - 1 || y == 1 || y == screenY + 1)
-                {
-                cout << "#";
-            }
-
-            else {
-                cout << " ";
-            }
-            
-           
-            
-        }
-        cout << endl;
-    }
-    
-}
-void newGameStart() {
-    cout << "Welcome to new game!\nPlease enter your character name:\n";
-    cin >> playerCharacterName;
-}
-void mainMenu() {
-    int playerInput;
-    cout << "Welcome to game!\n";
-    cout << "1.New Game\n";
-    cout << "2.Continue\n";
-    cout << "3.Settings\n";
-    cout << "4.Quit\n";
-
-    cin >> playerInput;
-
-    switch (playerInput)
-    {
-    case 1:
-        cout << "New Game!\n";
-        newGameStart();
-        break;
-    case 2: 
-        cout << "Continue!\n";
-        break;
-    case 3:
-        cout << "Setting!\n";
-    case 4:
-        cout << "Qiut!\n";
-    
-    default:
-        cout << "Invalid option!\n";
-        break;
-    }
-
-}
-
 class Hero {
-    public:
+public:
     string name;
     int hitPoints;
     int basicAttack;
     int basicArmor;
 };
 
-int main() {
-    mainMenu();
-    Hero Player;
+class Enemy {
+public:
+    string name;
+    int hitPoints;
+    int basicAttack;
+    int basicArmor;
+};
+
+Hero Player;
+Enemy Scamp;
+
+void printGameField() {
+    int screenX = 10;
+    int screenY = 20;
+    for (int x = 0; x < screenX; x++) {
+        for (int y = 0; y < screenY; y++) {
+            if (x == 1 || x == screenX - 1 || y == 1 || y == screenY - 1)
+                cout << "#";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void newGameStart() {
+    cout << "Welcome to new game!\nPlease enter your character name:\n";
+    cin >> playerCharacterName;
+}
+
+void initialisationSettings() {
     Player.name = playerCharacterName;
     Player.basicArmor = 13;
     Player.basicAttack = 12;
     Player.hitPoints = 30;
-    
-    
 
-    cout << "Your name is " << Player.name << endl;
-    printGameField();
+    Scamp.name = "Scamp";
+    Scamp.basicArmor = 4;
+    Scamp.basicAttack = 5;
+    Scamp.hitPoints = 10;
+}
+
+void battleFunc() {
+    bool EndBattle = true;
+    while (EndBattle) {
+        cout << playerCharacterName << ": " << Player.hitPoints << "      " << Scamp.name << ": " << Scamp.hitPoints << endl;
+        Player.hitPoints--;
+        if (Player.hitPoints <= 0 || Scamp.hitPoints <= 0)
+            EndBattle = false;
+    }
+}
+
+void mainMenu() {
+    int playerInput;
+    cout << "Welcome to game!\n";
+    cout << "1. New Game\n";
+    cout << "2. Continue\n";
+    cout << "3. Settings\n";
+    cout << "4. Quit\n";
+
+    cin >> playerInput;
+
+    switch (playerInput) {
+    case 1:
+        cout << "New Game!\n";
+        newGameStart();
+        initialisationSettings();
+        battleFunc();
+        break;
+    case 2:
+        cout << "Continue!\n";
+        break;
+    case 3:
+        cout << "Setting!\n";
+        break; // Don't forget to add break statements after each case
+    case 4:
+        cout << "Quit!\n";
+        break;
+    default:
+        cout << "Invalid option!\n";
+        break;
+    }
+}
+
+int main() {
+    mainMenu();
+    return 0;
 }
